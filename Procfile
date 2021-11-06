880038f990ab46fa6b6c05d43debe67cf7d4f449
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-11-06 09:42:29 +0100

    push

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn getdp_project.wsgi --log-file -
---
+++ mydp_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MydpAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mydp_app'
