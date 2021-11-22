8733217480bd03cf18fb356bc85a759be382c6b3
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-11-22 18:43:29 +0100

    npush

--- getdp_project/settings.py
+++ getdp_project/settings.py
@@ -137,12 +137,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
