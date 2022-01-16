df98e22e40c27b7beff521051e43ed8311bbf1d4
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-16 03:16:07 +0100

    push

--- mydp_app/tests/test_views.py
+++ mydp_app/tests/test_views.py
@@ -8,4 +8,27 @@ class TestViews(TestCase):
         client = Client()
         response = client.get(reverse('home'))
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'mydp_app/home.html', 'mydp_app/base.html', 'mydp_app/footer.html')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_login(self):
+        client = Client()
+        response = client.get(reverse('login'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_logout(self):
+        client = Client()
+        response = client.get(reverse('logout'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_register(self):
+        client = Client()
+        response = client.get(reverse('register'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
+
+    def test_create_banner(self):
+        client = Client()
+        response = client.get(reverse('create-banner'))
+        self.assertEquals(response.status_code, 302)
+        
