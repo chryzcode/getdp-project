99911c463fb956ffff5010729e0aaa5336176f99
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-16 13:35:58 +0100

    commit

--- mydp_app/tests/test_views.py
+++ mydp_app/tests/test_views.py
@@ -4,53 +4,56 @@ from mydp_app.models import *
 
 class TestViews(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('home')
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+        self.register_url = reverse('register')
+        self.create_banner_url = reverse('create-banner')
+        self.user_profile_url = reverse('user-profile', args=['CODE'])
+
     def test_home(self):
-        client = Client()
-        response = client.get(reverse('home'))
+        response = self.client.get(self.home_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
     def test_login(self):
-        client = Client()
-        response = client.get(reverse('login'))
+        response = self.client.get(self.login_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_logout(self):
-        client = Client()
-        response = client.get(reverse('logout'))
+        response = self.client.get(self.logout_url)
         self.assertEquals(response.status_code, 302)
 
     def test_register(self):
-        client = Client()
-        response = client.get(reverse('register'))
+        response = self.client.get(self.register_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'registration/register.html')
 
     def test_create_banner(self):
-        client = Client()
-        response = client.get(reverse('create-banner'))
+        response = self.client.get(self.create_banner_url)
         self.assertEquals(response.status_code, 302)
 
     def test_user_profile(self):
-        client = Client()
-        response = client.get(reverse('user-profile', args=['testuser']))
+        response = self.client.get(self.user_profile_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'user_profile.html')
 
-    def test_delete_account(self):
-        client = Client()
-        response = client.get(reverse('delete-account'))
-        self.assertEquals(response.status_code, 302)
+    # def test_delete_account(self):
+    #     client = Client()
+    #     response = client.get(reverse('delete-account'))
+    #     self.assertEquals(response.status_code, 302)
 
-    def test_categories(self):
-        client = Client()
-        response = client.get(reverse('categories'))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'categories.html')
+    # def test_categories(self):
+    #     client = Client()
+    #     response = client.get(reverse('categories'))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'categories.html')
 
-    def test_banner_category(self):
-        client = Client()
-        response = client.get(reverse('banner-category', args=['test']))
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'banner_category.html')
+    # def test_banner_category(self):
+    #     client = Client()
+    #     response = client.get(reverse('banner-category', args=['category_name']))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'banner_category.html')
