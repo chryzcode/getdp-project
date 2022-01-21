558c464d2a80d43ba651106bfbd669240cd966ad
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-21 23:45:00 +0100

    commit

--- functional_tests/test_project_home_page.py
+++ functional_tests/test_project_home_page.py
@@ -13,4 +13,7 @@ class TestProjectHomePage(StaticLiveServerTestCase):
 
     def test_no_projects_alert_is_displayed(self):
         self.browser.get(self.live_server_url)
-        time.sleep(20)
+        banner_grid = self.browser.find_element_by_class_name('banner-grid')
+        intro = self.browser.find_element_by_class_name('intro')
+        self.assertEquals(intro.find_element_by_tag_name('h2').text, 'Get Your DP Banner ---')
+        time.sleep(2000)
