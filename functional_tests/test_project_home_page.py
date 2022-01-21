7ae96c1001b7dc8332de1d1b4eca800fe13cf037
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-21 22:34:23 +0100

    commit

--- functional_tests/test_project_home_page.py
+++ functional_tests/test_project_home_page.py
@@ -2,7 +2,15 @@ from selenium import webdriver
 from mydp_app.models import *
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
+import time
 
 class TestProjectHomePage(StaticLiveServerTestCase):
-    def test_foo(self):
-        self.assertEquals(0, 1)
+    def setUp(self):
+        self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_no_projects_alert_is_displayed(self):
+        self.browser.get(self.live_server_url)
+        time.sleep(20)
