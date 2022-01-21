a41d1cb5975cbe7662f171a9824b22d55c2db264
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-21 23:25:24 +0100

    push

--- functional_tests/test_project_home_page.py
+++ functional_tests/test_project_home_page.py
@@ -1,16 +1,16 @@
-# from selenium import webdriver
-# from mydp_app.models import *
-# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-# from django.urls import reverse
-# import time
+from selenium import webdriver
+from mydp_app.models import *
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+import time
 
-# class TestProjectHomePage(StaticLiveServerTestCase):
-#     def setUp(self):
-#         self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+class TestProjectHomePage(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
 
-#     def tearDown(self):
-#         self.browser.close()
+    def tearDown(self):
+        self.browser.close()
 
-#     def test_no_projects_alert_is_displayed(self):
-#         self.browser.get(self.live_server_url)
-#         time.sleep(20)
+    def test_no_projects_alert_is_displayed(self):
+        self.browser.get(self.live_server_url)
+        time.sleep(20)
