a85c33b99a22228e3dba4690ef822513ee702d1c
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-23 13:01:32 +0100

    commit

--- functional_tests/test_project_home_page.py
+++ functional_tests/test_project_home_page.py
@@ -19,12 +19,15 @@ class TestProjectHomePage(StaticLiveServerTestCase):
         self.assertEquals(intro.find_element(By.TAG_NAME, 'h2').text, 'Get Your DP Banner')
         time.sleep(10)
 
-    # def test_click_banner_redirect_to_view_page(self):
-    #     self.browser.get(self.live_server_url)
-    #     view_banner_url = self.live_server_url + reverse('view-banner', kwargs={'slug': 'testing-banner'})
-    #     banner = self.browser.find_element(By.CLASS_NAME, 'banner-grid')
-    #     banner.find_element(By.CLASS_NAME, 'banner-link').click()
-    #     self.assertEquals(self.browser.current_url, view_banner_url)
+  
+
+    #selenium test for click banner redirect to view page
+    def test_click_banner_redirect_to_view_page(self):
+        self.browser.get(self.live_server_url)
+        banner = self.browser.find_element(By.CLASS_NAME, 'banner')
+        banner.click()
+        time.sleep(10)
+        self.assertEquals(self.browser.current_url, self.live_server_url + '/banner/' + self.banner.slug)
 
 
     
