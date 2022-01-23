a4207ee804c4428cc3d0a6a8f0db12fefc1436bb
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-23 12:57:27 +0100

    commit

--- functional_tests/test_project_home_page.py
+++ functional_tests/test_project_home_page.py
@@ -8,7 +8,7 @@ import time
 
 class TestProjectHomePage(StaticLiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
 
     def tearDown(self):
         self.browser.close()
@@ -17,13 +17,14 @@ class TestProjectHomePage(StaticLiveServerTestCase):
         self.browser.get(self.live_server_url)
         intro = self.browser.find_element(By.CLASS_NAME, 'intro')
         self.assertEquals(intro.find_element(By.TAG_NAME, 'h2').text, 'Get Your DP Banner')
+        time.sleep(10)
 
-    def test_click_banner_redirect_to_view_page(self):
-        self.browser.get(self.live_server_url)
-        view_banner_url = self.live_server_url + reverse('view-banner', kwargs={'slug': 'testing-banner'})
-        banner = self.browser.find_element(By.CLASS_NAME, 'banner-grid')
-        banner.find_element(By.CLASS_NAME, 'banner-link').click()
-        self.assertEquals(self.browser.current_url, view_banner_url)
+    # def test_click_banner_redirect_to_view_page(self):
+    #     self.browser.get(self.live_server_url)
+    #     view_banner_url = self.live_server_url + reverse('view-banner', kwargs={'slug': 'testing-banner'})
+    #     banner = self.browser.find_element(By.CLASS_NAME, 'banner-grid')
+    #     banner.find_element(By.CLASS_NAME, 'banner-link').click()
+    #     self.assertEquals(self.browser.current_url, view_banner_url)
 
 
     
