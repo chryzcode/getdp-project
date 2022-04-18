4f8ddf2bc5fd0d6809000612ce75f8fbaf95c2c5
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-04-18 17:13:17 +0100

    push

--- mydp_app/forms.py
+++ mydp_app/forms.py
@@ -1,125 +1,125 @@
-# from django.forms import ModelForm
-# from django import forms
-# from .models import *
-# from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django import forms
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
 
 
-# class SignupForm(UserCreationForm):
-#     full_name = forms.CharField(max_length=100)
-#     email = forms.EmailField()
+class SignupForm(UserCreationForm):
+    full_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
 
-#     class Meta:
-#         model = User
-#         fields = ("full_name", "username", "email", "password1", "password2")
+    class Meta:
+        model = User
+        fields = ("full_name", "username", "email", "password1", "password2")
 
-#     def __init__(self, *args, **kwargs):
-#         super(SignupForm, self).__init__(*args, **kwargs)
-
-
-# class BannerForm(ModelForm):
-#     class Meta:
-#         model = Banner
-#         fields = ("name", "description", "category", "tag", "image")
-#         exclude = ["user", "slug", "banner_users"]
-
-#         widgets = {
-#             "name": forms.TextInput(attrs={"class": "form-control"}),
-#             "category": forms.Select(
-#                 choices=Category.objects.all().values_list("name", "name"),
-#                 attrs={"class": "form-control"},
-#             ),
-#             "tag": forms.Select(
-#                 choices=Tag.objects.all().values_list("name", "name"),
-#                 attrs={"class": "form-control"},
-#             ),
-#         }
-
-#     def __init__(self, *args, **kwargs):
-#         super(BannerForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
 
 
-# class UserBannerForm(ModelForm):
-#     class Meta:
-#         model = UserBanner
-#         fields = ["image", "full_name"]
-#         exclude = ["user", "banner"]
+class BannerForm(ModelForm):
+    class Meta:
+        model = Banner
+        fields = ("name", "description", "category", "tag", "image")
+        exclude = ["user", "slug", "banner_users"]
 
-#         widgets = {
-#             "full_name": forms.TextInput(
-#                 attrs={"class": "form-control", "placeholder": "Put in your name"}
-#             ),
-#             "image": forms.FileInput(attrs={"class": "form-control"}),
-#         }
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "category": forms.Select(
+                choices=Category.objects.all().values_list("name", "name"),
+                attrs={"class": "form-control"},
+            ),
+            "tag": forms.Select(
+                choices=Tag.objects.all().values_list("name", "name"),
+                attrs={"class": "form-control"},
+            ),
+        }
 
-#     def __init__(self, *args, **kwargs):
-#         super(UserBannerForm, self).__init__(*args, **kwargs)
-
-
-# class CommentForm(ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ["name"]
-#         widgets = {
-#             "name": forms.TextInput(
-#                 attrs={
-#                     "class": "form-control",
-#                     "placeholder": "Put in your comments.....",
-#                 }
-#             ),
-#         }
-#         labels = {
-#             "name": (""),
-#         }
-
-#     def __init__(self, *args, **kwargs):
-#         super(CommentForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(BannerForm, self).__init__(*args, **kwargs)
 
 
-# class UserProfileForm(ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ["avatar", "full_name", "username"]
+class UserBannerForm(ModelForm):
+    class Meta:
+        model = UserBanner
+        fields = ["image", "full_name"]
+        exclude = ["user", "banner"]
 
-#         widgets = {
-#             "avatar": forms.FileInput(attrs={"class": "form-control"}),
-#             "full_name": forms.TextInput(
-#                 attrs={"class": "form-control", "placeholder": "Full Name"}
-#             ),
-#             "username": forms.TextInput(
-#                 attrs={"class": "form-control", "placeholder": "Username"}
-#             ),
-#         }
+        widgets = {
+            "full_name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Put in your name"}
+            ),
+            "image": forms.FileInput(attrs={"class": "form-control"}),
+        }
 
-#     def __init__(self, *args, **kwargs):
-#         super(UserProfileForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(UserBannerForm, self).__init__(*args, **kwargs)
 
 
-# # class PasswordChangeForm(SetPasswordForm):
-# #     """
-# #     A form that lets a user change their password by entering their old
-# #     password.
-# #     """
-# #     error_messages = dict(SetPasswordForm.error_messages, **{
-# #         'password_incorrect': _("Your old password was entered incorrectly. "
-# #                                 "Please enter it again."),
-# #     })
-# #     old_password = forms.CharField(label=_("Old password"),
-# #                                    widget=forms.PasswordInput)
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["name"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Put in your comments.....",
+                }
+            ),
+        }
+        labels = {
+            "name": (""),
+        }
 
-# #     def clean_old_password(self):
-# #         """
-# #         Validates that the old_password field is correct.
-# #         """
-# #         old_password = self.cleaned_data["old_password"]
-# #         if not self.user.check_password(old_password):
-# #             raise forms.ValidationError(
-# #                 self.error_messages['password_incorrect'],
-# #                 code='password_incorrect',
-# #             )
-# #         return old_password
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
 
 
-# # PasswordChangeForm.base_fields = OrderedDict(
-# #     (k, PasswordChangeForm.base_fields[k])
-# #     for k in ['old_password', 'new_password1', 'new_password2']
-# # )
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["avatar", "full_name", "username"]
+
+        widgets = {
+            "avatar": forms.FileInput(attrs={"class": "form-control"}),
+            "full_name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Full Name"}
+            ),
+            "username": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Username"}
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+
+
+# class PasswordChangeForm(SetPasswordForm):
+#     """
+#     A form that lets a user change their password by entering their old
+#     password.
+#     """
+#     error_messages = dict(SetPasswordForm.error_messages, **{
+#         'password_incorrect': _("Your old password was entered incorrectly. "
+#                                 "Please enter it again."),
+#     })
+#     old_password = forms.CharField(label=_("Old password"),
+#                                    widget=forms.PasswordInput)
+
+#     def clean_old_password(self):
+#         """
+#         Validates that the old_password field is correct.
+#         """
+#         old_password = self.cleaned_data["old_password"]
+#         if not self.user.check_password(old_password):
+#             raise forms.ValidationError(
+#                 self.error_messages['password_incorrect'],
+#                 code='password_incorrect',
+#             )
+#         return old_password
+
+
+# PasswordChangeForm.base_fields = OrderedDict(
+#     (k, PasswordChangeForm.base_fields[k])
+#     for k in ['old_password', 'new_password1', 'new_password2']
+# )
