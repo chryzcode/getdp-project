aa69fa5576ad6848df4c55e857d06bbc21137934
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-26 13:04:06 +0100

    push

--- getdp_project/settings.py
+++ getdp_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.environ.get("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["get-my-dp.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["getmydp.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
