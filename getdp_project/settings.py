9028c1a90dcc12fd17da1ffbdca4532acf03cb27
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-09-26 13:31:49 +0100

    push

--- getdp_project/settings.py
+++ getdp_project/settings.py
@@ -77,17 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "getdp_project.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 AUTH_USER_MODEL = "mydp_app.User"
 
 
